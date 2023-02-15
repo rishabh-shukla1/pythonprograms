97cde1458cbf7d0971702bafa24a74783f480d8d
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-02-15 17:01:05 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,7 @@
+#input section
+n=eval(input("enter first number:"))
+m=eval(input("enter second number:"))
+# logic section
+sum=n.__add__(m)
+#display section
+print(f'sum of{n} ans {m} is {sum}')
