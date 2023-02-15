431d86c9af73c7aabc72f2c62c6dc6c7845b85bc
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-02-15 17:01:20 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,8 @@
+# write a python script to claculate the area of a circle where area is entered by the user
+import math
+#input sectio
+r=eval(input("enter radius:"))
+# logic section
+result=math.pi*r*r
+#display section
+print(f"area of a circle{result:2f}")
